a3c0624fa10a2604b4ee49eafffe620b1db3be4e
Ian Lohan <102693640+Ian-Lohan@users.noreply.github.com>
2023-04-09 15:24:31 -0300

    Update script.js

--- to-do list/script.js
+++ to-do list/script.js
@@ -1,4 +1,4 @@
-// Seleção de elementos
+// Seleção de Elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
